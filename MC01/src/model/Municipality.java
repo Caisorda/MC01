2cb3ad53150ae983e44d341a773cc413f72778e9
cutiearvin <arvin_medina1996@yahoo.com>
2016-02-26 20:21:52 +0800

    Added municipality

---
+++ MC01/src/model/Municipality.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class Municipality {
+    
+    private String munNum;
+    
+    public Municipality(String munNum){
+        this.munNum = munNum;
+    }
+
+    public String getMunNum() {
+        return munNum;
+    }
+
+    public void setMunNum(String munNum) {
+        this.munNum = munNum;
+    }
+    
+    
+    
+}
