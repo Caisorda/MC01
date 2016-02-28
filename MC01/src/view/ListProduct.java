f575ebb13da375f942dc085dfb75f974509c1ce4
Caisorda <juan_salceda@dlsu.edu.ph>
2016-02-28 14:01:24 +0800

    Revert "ListProduct query integration"
    This reverts commit a83c777ff758d4ff093c31d066b899dce1b8e4a3.

--- MC01/src/view/ListProduct.java
+++ MC01/src/view/ListProduct.java
@@ -2,11 +2,6 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,47 +15,43 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import controller.ListProductController;
-import controller.MunicipalityController;
-import model.Municipality;
-
-public class ListProduct extends JFrame{
+public class ListProduct extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
-	private DefaultTableModel model;
-	private JScrollPane scrollPane;
-	private ListProductController listProductController;
-	
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					ListProduct frame = new ListProduct();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-	
-	public ListProduct(){
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListProduct frame = new ListProduct();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListProduct() {
 		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 150, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-
-		String[] muns = new String[]{"1", "2", "3", "4", "5", "6", "7", 
-									 "8", "9", "10", "11", "12", "13", 
-									 "14", "15", "16", "17", "18", "19", 
-									 "20", "21", "22", "23", "24"};
-		JComboBox cbProduce = new JComboBox(muns);
-		//subject to change
+		
+		JComboBox cbProduce = new JComboBox();
+		
 		JLabel lblSelectProduce = new JLabel("Select Town");
 		
-		scrollPane = new JScrollPane();
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -84,58 +75,16 @@ public class ListProduct extends JFrame{
 					.addContainerGap())
 		);
 		
-		this.table = new JTable();
-		this.model = new DefaultTableModel() {
-
-			public boolean isFocusable(int rowIndex, int mColIndex) {
-				return false;
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Produce"
 			}
-
-			public boolean isCellSelectable(int rowIndex, int mColIndex) {
-				return false;
-			}
-		};
-        this.model.setColumnIdentifiers(new String[]{"Produce"});
-        this.table.setModel(model);
+		));
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
-		
-		cbProduce.addItemListener(new ItemListener(){
-            @Override
-            public void itemStateChanged(ItemEvent event) {
-                updateTable((String)cbProduce.getSelectedItem());
-            }
-        });
-		listProductController = new ListProductController();
-		updateTable((String)cbProduce.getSelectedItem());
-	}
-	
-	public void clearTable(){
-        
-		for (int i = 0; i < this.model.getRowCount(); i++) {
-			for (int j = 0; j < this.model.getColumnCount(); j++) {
-				this.model.setValueAt(null, i, j);
-			}
-			this.model.removeRow(i);
-		}
-		
-		this.model.setRowCount(0);
-		this.model.getDataVector().removeAllElements();
-		this.model.fireTableDataChanged(); 
-	} 
-	
-	public void updateTable(String town){
-		ArrayList<String> products = new ArrayList();
-		products.addAll(listProductController.get(town));
-        clearTable();
-        for(String product: products){
-        	if(product!=null&&!product.equals("")){
-        		model.setRowCount(model.getRowCount() + 1);
-        		model.setValueAt(product, model.getRowCount() - 1, 0);
-        	}
-        }
-        scrollPane.repaint();
-        scrollPane.revalidate();
 	}
 }
 
